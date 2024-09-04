c8a8f4447a7f2eeb3c319448081cabdca7196403
Paweł Wawrzyniak <pawel.adam.wawrzyniak@gmail.com>
2024-09-04 10:57:03 +0200

    add first test

--- tests/UserValidatorTest.php
+++ tests/UserValidatorTest.php
@@ -9,6 +9,19 @@ use App\UserValidator;
 
 class UserValidatorTest extends TestCase
 {
+    #[DataProvider('ValidationPasswordDataProvider')]
+    public function testValidatePassword(string $password, bool $expected): void
+    {
+        // Given
+        $validator = new UserValidator();
+
+        // When
+        $result = $validator->validatePassword($password);
+        // Then
+
+        $this->assertSame($expected,$result);
+    }
+
     #[DataProvider('ValidationEmailDataProvider')]
     public function testValidateEmail(string $email, bool $expected): void
     {
@@ -21,6 +34,15 @@ class UserValidatorTest extends TestCase
 
         $this->assertSame($expected,$result);
     }
+
+    public static function ValidationPasswordDataProvider(): iterable
+    {
+        yield 'correct password' => [
+            'password' => 'Wl@12345',
+            'expected' => true,
+        ];
+    }
+
 
     public static function ValidationEmailDataProvider():iterable
     {
